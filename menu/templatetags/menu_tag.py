740aa43e760b07ea9d7b62978c67bace780483b6
DBWtv <dima.bwc@gmail.com>
2023-02-22 17:35:24 +0600

    for many menu

--- menu/templatetags/menu_tag.py
+++ menu/templatetags/menu_tag.py
@@ -5,7 +5,8 @@ register = template.Library()
 
 @register.simple_tag
 def draw_menu(menu_slug,*args, **kwargs):
-    print( f'slug: {menu_slug}')
-    menu_items = MenuItem.objects.filter(slug = menu_slug)
-    print(menu_items)
+    if menu_slug == None:
+        menu_items = MenuItem.objects.filter(parent = None)
+    else:
+        menu_items = MenuItem.objects.filter(slug = menu_slug)
     return menu_items
--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from .models import MenuItem
 
-def start_page(request, url='main-menu'):
+def start_page(request, url=None):
     return render(request, 'menu.html', {'menu_name': url})
